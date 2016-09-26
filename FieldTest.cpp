c0cfa4a85672c67667d47c0e1c9c140b544b27e1
MarshallDyer <mdyer7@mail.csuchico.edu>
2016-09-26 11:30:35 -0700

    added a few tests, discovered one bug

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
+
 class FieldTest : public ::testing::Test
 {
 	protected:
@@ -18,7 +18,47 @@ class FieldTest : public ::testing::Test
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
-	
+
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+  minefield.placeMine(9,9);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(9,9) );
+  minefield.placeMine(0,0);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(0,0) );
+  minefield.placeMine(0,9);
+  ASSERT_EQ( MINE_HIDDEN, minefield.get(0,9) );
+  minefield.placeMine(9,0);
+  ASSERT_EQ( MINE_HIDDEN, minefield.get(9,0) );
+}
+
+TEST(FieldTest, constructorwithoutArgument)
+{
+	Field minefield;
+
+	minefield.placeMine(9, 9);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(9, 9) );
+}
+
+TEST(FieldTest, constructorWithArgument)
+{
+	Field minefield(20);
+
+	minefield.placeMine(19, 19);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(19, 19) );
+}
+
+TEST(FieldTest, RevealAdjacentAtEdge)
+{
+	Field minefield;
+
+	minefield.placeMine(4,5);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+  minefield.placeMine(9,9);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(9,9) );
+  minefield.placeMine(0,0);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(0,0) );
+  minefield.placeMine(0,9);
+  ASSERT_EQ( MINE_HIDDEN, minefield.get(0,9) );
+  minefield.placeMine(9,0);
+  ASSERT_EQ( MINE_HIDDEN, minefield.get(9,0) );
 }
--- buglog.md
+++ buglog.md
@@ -1,1 +1,10 @@
 # BUG LOG
+
+[  FAILED  ] FieldTest.constructorWithArgument
+// This test fails because when you call the constructor for
+Field with an argument, such as 20 in this case, it will make
+the map the correct size, but it doesn't store the size in
+FIELD_DIMENSION (and it couldn't anyway because it's const).
+When placing a mine at 19, 19, which should be in bounds, it
+fails because it checks against FIELD_DIMENSION, which is by
+default 10.
